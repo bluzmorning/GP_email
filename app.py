c43dec1e64cbf2327950b7dbb2cac2ef73f25157
alko0314 <konovalcev@me.com>
2015-08-06 13:24:15 +0300

    123

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def do_connect():
     server = config.get("parameters", "sqlserver")
     username = config.get("parameters", "login")
     password = config.get("parameters", "password")
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE=Dynamics;UID='+username+';PWD='+password+'')
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';UID='+username+';PWD='+password+'')
     users_query = cnxn.cursor()
     users_query.execute("select USERID from SY01400")
     data_users = users_query.fetchall()
