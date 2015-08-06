99aeefa0cb5229e6fe6a9c2066914b1e80d8dddd
alko0314 <konovalcev@me.com>
2015-08-06 13:02:48 +0300

    123

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-hello
+import pyodbc
+import base64
+import ConfigParser
+def do_connect():
+    config = ConfigParser.ConfigParser()
+    config.read("config.ini")
+    server = config.get("parameters", "sqlserver")
+    username = config.get("parameters", "login")
+    password = config.get("parameters", "password")
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE=Dynamics;UID='+username+';PWD='+password+'')
+    users_query = cnxn.cursor()
+    users_query.execute("select USERID from SY01400")
+    data_users = users_query.fetchall()
+    users_query.close()
+    print data_users
+do_connect()
