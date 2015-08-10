5862dc6bea7a29b9fb2875dd5ecfbcb1a6356379
alko0314 <konovalcev@me.com>
2015-08-10 11:42:51 +0300

    123

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import pyodbc
 import ConfigParser
 import pandas as pd
+import datetime
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from smtplib import SMTP
+today = str(datetime.date.today())
 def do_connect():
     config = ConfigParser.ConfigParser()
     config.read("config.ini")
@@ -13,8 +15,8 @@ def do_connect():
     password = config.get("parameters", "password")
     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE=Dynamics;UID='+username+';PWD='+password+'')
     users_query = cnxn.cursor()
-    df = pd.read_sql_query("exec FSSG_wrong_accounttype_report", cnxn)
-    writer = pd.ExcelWriter('accounts.xlsx')
+    df = pd.read_sql_query("exec FSSG_wrong_accounttype_report", cnxn, index_col=None)
+    writer = pd.ExcelWriter('wrong_accounts_'+today+'.xlsx')
     df.to_excel(writer, sheet_name='account codes in GP')
     writer.sheets['account codes in GP'].column_dimensions['A'].width = 10
     writer.sheets['account codes in GP'].column_dimensions['B'].width = 50
@@ -27,15 +29,18 @@ def do_sendemail():
     config.read("config.ini")
     msg = MIMEMultipart()
     recipients = config.get("parameters", "recipients")
-    msg['Subject'] = 'Wrong accounts type in Dynamics GP (monthly report)'
-    msg['From'] = 'financialsystemssupportgroup@netcracker.com'
-    msg['To'] = ", ".join([recipients])
+    recipients1 = recipients.split(',')
+    msg['Subject'] = "Wrong accounts type in Dynamics GP (monthly report)"
+    msg['From'] = "FinancialSystemsSupportGroup@NetCracker.com"
+    msg['To'] = ", ".join(recipients1)
     msg.preamble = 'Multipart massage.\n'
     part = MIMEText("Hello, team. Please, pay your attention on wrong account type in Dynamics GP. This message has been sent automatically. Thanks.")
     msg.attach(part)
-    part = MIMEApplication(open("accounts.xlsx","rb").read())
-    part.add_header('Content-Disposition', 'attachment', filename="accounts.xlsx")
+    part = MIMEApplication(open('wrong_accounts_'+today+'.xlsx','rb').read())
+    part.add_header('Content-Disposition', 'attachment', filename='wrong_accounts_'+today+'.xlsx')
     msg.attach(part)
     smtp = SMTP("iplanet", 25)
     smtp.ehlo()
-    smtp.sendmail(msg['From'], recipients, msg.as_string())
+    smtp.sendmail(msg['From'], recipients1, msg.as_string())
+do_connect()
+do_sendemail()
